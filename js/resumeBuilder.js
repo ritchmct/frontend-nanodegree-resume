7151d8cd6beab47f8504893aaf058402cf6b0255
Ritchie <ritchie_diaper@yahoo.co.uk>
2016-01-05 12:46:05 +0000

    Add work experience

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -77,3 +77,50 @@ bio.display = function() {
 }
 
 bio.display();
+
+// Work experience
+
+var work = {
+	"jobs": [{
+		"employer": "IBM",
+		"title": "IT Specialist",
+		"location": "Edinburgh, UK",
+		"dates": "2000-2015",
+		"description": "Working with IBM customers to deploy IBM software solutions."
+	}, {
+		"employer": "Self Employed",
+		"title": "IT Consultant",
+		"location": "London, UK",
+		"dates": "1997-2000",
+		"description": "Deploying Systems Management software at UK airports."
+	}, {
+		"employer": "Schlumberger",
+		"title": "Computer Engineer",
+		"location": "London, UK",
+		"dates": "1990-1997",
+		"description": "Maintaining computer hardware at Schlumberger data centers."
+	}, {
+		"employer": "Seismograph Service Limited",
+		"title": "Field Seismologist",
+		"location": "Port Harcourt, Nigeria",
+		"dates": "1985-1990",
+		"description": "Working on a shallow marine Seismic crew in the Niger delta involved in oil exploration."
+	}]
+};
+
+work.display = function() {
+	work.jobs.forEach(function(job) {
+		$("#workExperience").append(HTMLworkStart);
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", job.employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", job.title);
+		$(".work-entry:last").append(formattedEmployer + formattedTitle);
+		var formattedDates = HTMLworkDates.replace("%data%", job.dates);
+		$(".work-entry:last").append(formattedDates);
+		var formattedLocation = HTMLworkLocation.replace("%data%", job.location);
+		$(".work-entry:last").append(formattedLocation);
+		var formattedDescription = HTMLworkDescription.replace("%data%", job.description);
+		$(".work-entry:last").append(formattedDescription);
+	});
+}
+
+work.display();
